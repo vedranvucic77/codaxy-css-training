fef0a14ee2ddfa4180edb14209465d1174912031
vedranvucic77 <vedranvucic77@codaxy.com>
2022-04-06 12:02:29 +0200

    responsive

--- app/components/random-text-card/index.js
+++ app/components/random-text-card/index.js
@@ -2,7 +2,7 @@ import { Button } from "cx/widgets";
 
 export const RandomTextCard = ({ title, desc, btn }) => (
     <cx>
-        <div class="randomtextcard">
+        <div class="randomtextcard flex-col">
             <h2 class="randomtextcard_title title">{title}</h2>
             <p class="randomtextcard_description line-clamp-2">{desc}</p>
             <Button mod="hollow" class="randomtextcard_readmore">
